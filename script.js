24c46d564bba80659629f9e306f11eed95b320b2
Omar Adel <oadel@kn-it.com>
2022-03-16 15:42:38 +0200

    refactoring the code & closing it

--- script.js
+++ script.js
@@ -5,24 +5,26 @@ const modalBtn = document.querySelector('.modal');
 const modalCloseBtn = document.querySelector('.close-modal');
 const overlay = document.querySelector('.overlay');
 
-function removeHiddenClass() {
+const removeHiddenClass = function () {
   modalBtn.classList.remove('hidden');
   overlay.classList.remove('hidden');
-}
+};
 
-function addHiddenClass() {
+const addHiddenClass = function () {
   modalBtn.classList.add('hidden');
   overlay.classList.add('hidden');
-}
+};
 
 for (let i = 0; i < modalOpenBtns.length; i++) {
-  modalOpenBtns[i].addEventListener('click', () => removeHiddenClass());
+  modalOpenBtns[i].addEventListener('click', removeHiddenClass);
 }
 
-modalCloseBtn.addEventListener('click', () => addHiddenClass());
+modalCloseBtn.addEventListener('click', addHiddenClass);
 
-overlay.addEventListener('click', () => addHiddenClass());
+overlay.addEventListener('click', addHiddenClass);
 
-document.addEventListener('keydown', event =>
-  event.key === 'Escape' ? addHiddenClass() : removeHiddenClass()
-);
+document.addEventListener('keydown', function (event) {
+  if (event.key === 'Escape' && !modalBtn.classList.contains('hidden')) {
+    addHiddenClass();
+  }
+});
