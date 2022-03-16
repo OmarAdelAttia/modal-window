485de9a2beaf4923859920ea20387d1be2b5cf6d
Omar Adel <oadel@kn-it.com>
2022-03-16 12:09:15 +0200

    adding events & removing

--- script.js
+++ script.js
@@ -5,7 +5,24 @@ const modalBtn = document.querySelector('.modal');
 const modalCloseBtn = document.querySelector('.close-modal');
 const overlay = document.querySelector('.overlay');
 
-for (let i = 0; i < modalOpenBtns.length; i++) {
-  console.log(modalOpenBtns[i].textContent);
+function removeHiddenClass() {
+  modalBtn.classList.remove('hidden');
+  overlay.classList.remove('hidden');
 }
 
+function addHiddenClass() {
+  modalBtn.classList.add('hidden');
+  overlay.classList.add('hidden');
+}
+
+for (let i = 0; i < modalOpenBtns.length; i++) {
+  modalOpenBtns[i].addEventListener('click', () => removeHiddenClass());
+}
+
+modalCloseBtn.addEventListener('click', () => addHiddenClass());
+
+overlay.addEventListener('click', () => addHiddenClass());
+
+document.addEventListener('keydown', event =>
+  event.key === 'Escape' ? addHiddenClass() : removeHiddenClass()
+);
